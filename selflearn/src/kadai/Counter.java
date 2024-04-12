4143886ba8b03d8b0733e4f25a5a7d4b5ecb42fd
五藤沙知 <@sachi>
2024-04-12 10:53:07 +0900

    refactored my code

--- selflearn/src/kadai/Counter.java
+++ selflearn/src/kadai/Counter.java
@@ -2,34 +2,37 @@ package kadai;
 
 public class Counter {
 
-    private int wins;
-    private int loses;
-    private int draws;
+	private int wins;
+	private int loses;
+	private int draws;
 
-    public Counter() {
-        this.wins = 0;
-        this.loses = 0;
-        this.draws = 0;
-    }
+	public Counter() {
+		this.wins = 0;
+		this.loses = 0;
+		this.draws = 0;
+	}
 
-    // Getter methods
+	// Getter methods
 
-    public int getWins() {
-        return this.wins;
-    }
+	public int getWins() {
+		return this.wins;
+	}
 
-    public int getLoses() {
-        return this.loses;
-    }
+	public int getLoses() {
+		return this.loses;
+	}
 
-    public int getDraws() {
-        return this.draws;
-    }
+	public int getDraws() {
+		return this.draws;
+	}
 
-    public void setCount(String result) {
-        if (result.equals("win")) this.wins = this.wins + 1;
-        else if (result.equals("lose")) this.loses++;
-        else this.draws++;
-    }
+	public void setCount(String result) {
+		if (result.equals("win"))
+			this.wins++;
+		else if (result.equals("lose"))
+			this.loses++;
+		else
+			this.draws++;
+	}
 
 }
